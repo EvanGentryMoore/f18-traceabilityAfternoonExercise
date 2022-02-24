d14b9cf7d557b4ccf6502038c9615dbb64c4c5a7
Evan Moore <even.gentry.moore@gmail.com>
2022-02-24 16:05:48 -0700

    Attempt Try Catch 3.2 - forgot to tak out attempt 2 in server/index.js

--- server/index.js
+++ server/index.js
@@ -9,16 +9,6 @@ const rollbar = new Rollbar({
   captureUnhandledRejections: true
 });
 
-rollbar.try {
-    nonExistentFunction();
-  } catch (error) {
-    console.error(error);
-    // expected output: ReferenceError: nonExistentFunction is not defined
-    // Note - error messages will vary depending on browser
-  }
-)
-  
-
 const app = express()
 
 
