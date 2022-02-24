6aa5239dfc8174beeb37edd3903278c67275b94f
Evan Moore <even.gentry.moore@gmail.com>
2022-02-24 15:46:01 -0700

    Added Rollbar

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const path = require('path')
 
+// include and initialize the rollbar library with your access token
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: 'bdeefe80bc24430a949d62816c5f9580',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+
 const app = express()
 
 
@@ -8,6 +18,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+rollbar.log("Hello world!");
 
 const port = process.env.PORT || 8534
 
