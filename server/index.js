6566654a59177059df73e51f26eceb3fc0cb3049
Evan Moore <even.gentry.moore@gmail.com>
2022-02-24 16:04:18 -0700

    Attempt Try Catch 3

--- server/index.js
+++ server/index.js
@@ -26,6 +26,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+app.get('/fail', (req, res) => {
+    try {
+        nonExistentFunction();
+      } catch (error) {
+        console.error(error);
+        // expected output: ReferenceError: nonExistentFunction is not defined
+        // Note - error messages will vary depending on browser
+      }
+})
+
 const port = process.env.PORT || 8534
 
 app.listen(port, () => console.log(`Limping and skimping on ${port}`))
